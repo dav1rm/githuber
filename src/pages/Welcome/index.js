359b54012032386cbcdead83823fc05367b833b6
Davi <davirodrigues8@gmail.com>
2019-03-11 21:44:50 -0300

    Adiciona loading e error no login

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import api from '~/services/api';
 
 import {
-  View, Text, TextInput, TouchableOpacity, StatusBar, AsyncStorage,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  AsyncStorage,
+  ActivityIndicator,
 } from 'react-native';
 import styles from './styles';
 
 export default class Welcome extends Component {
   state = {
     username: '',
+    loading: false,
+    error: false,
   };
 
   checkUserExist = async (username) => {
@@ -25,18 +33,20 @@ export default class Welcome extends Component {
     const { username } = this.state;
     const { navigation } = this.props;
 
+    this.setState({ loading: true });
+
     try {
       await this.checkUserExist(username);
       await this.saveUser(username);
 
       navigation.navigate('Repositories');
     } catch (err) {
-      console.tron.log('Usuário não existe');
+      this.setState({ loading: false, error: true });
     }
   };
 
   render() {
-    const { username } = this.state;
+    const { username, loading, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" backgroundColor={styles.statusBar.backgroundColor} />
@@ -45,6 +55,7 @@ export default class Welcome extends Component {
           Para continuar precisamos que você informe seu usuário no Github.
         </Text>
 
+        {error && <Text style={styles.error}>Usuário inexistente.</Text>}
         <View style={styles.form}>
           <TextInput
             style={styles.input}
@@ -57,7 +68,11 @@ export default class Welcome extends Component {
           />
 
           <TouchableOpacity style={styles.button} onPress={this.signIn}>
-            <Text style={styles.buttonText}>Prosseguir</Text>
+            {loading ? (
+              <ActivityIndicator size="small" color="#FFF" />
+            ) : (
+              <Text style={styles.buttonText}>Prosseguir</Text>
+            )}
           </TouchableOpacity>
         </View>
       </View>
