f389725baf6b00689409f00add793c78943c201b
Davi <davirodrigues8@gmail.com>
2019-03-11 22:45:46 -0300

    Adiciona header e prop-types

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import Header from '~/components/Header';
 // import styles from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Repositories</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
