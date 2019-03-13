4873d9407fd5796e208f9f86f1346f0ae07bbe79
Davi <davirodrigues8@gmail.com>
2019-03-13 07:27:42 -0300

    Busca repositorios e adiciona loading

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 
-import { View } from 'react-native';
+import api from '~/services/api';
+
+import {
+  View, AsyncStorage, Text, ActivityIndicator,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
-// import styles from './styles';
-
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
-);
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
 TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
+
+class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
+
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderList = () => (
+    <Text>Lista</Text>
+  );
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View>
+        <Header title="Repositórios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
 
 Repositories.navigationOptions = {
   tabBarIcon: TabIcon,
